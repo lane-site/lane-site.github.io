509c829bc197d5a6b012ab6daf4cfce2c4cea3d0
DasPrombelBread <82669166+DasPrombelBread@users.noreply.github.com>
2021-08-03 09:04:59 +0200

    Create buttons.js

---
+++ style/buttons.js
@@ -0,0 +1,20 @@
+function twitter() {
+  window.open(
+              "https://twitter.com/das_bread", "_blank");
+}
+function contact() {
+  location.replace("https://fh-site.github.io/info/contact.html ")
+}
+function home() {
+  location.replace("https://fh-site.github.io/additional-stuff/whoops.html")
+}
+function legal() {
+  location.replace("https://fh-site.github.io/info/legal.html ")
+}
+function projects() {
+  location.replace("https://fh-site.github.io/info/projects.html ")
+}
+function github() {
+  window.open(
+              "https://github.com/DasPrombelBread", "_blank");
+}
