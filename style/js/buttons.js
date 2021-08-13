a32243cd71d3b93120f32092caacb03379e594e4
DasPrombelBread <82669166+DasPrombelBread@users.noreply.github.com>
2021-08-13 17:39:19 +0200

    Add files via upload

---
+++ style/js/buttons.js
@@ -0,0 +1,27 @@
+function twitter() {
+  window.open(
+              "https://twitter.com/das_bread", "_blank");
+}
+function contact() {
+  location.replace("https://fh-site.github.io/info/contact.html ")
+}
+function blog() {
+  location.replace("https://fh-site.github.io/blog/index.html ")
+}
+function home() {
+  location.replace("https://fh-site.github.io/home.html")
+}
+function home_rr() {
+  window.open(
+              "https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
+}
+function legal() {
+  location.replace("https://fh-site.github.io/info/legal.html ")
+}
+function projects() {
+  location.replace("https://fh-site.github.io/info/projects.html ")
+}
+function github() {
+  window.open(
+              "https://github.com/DasPrombelBread", "_blank");
+}
