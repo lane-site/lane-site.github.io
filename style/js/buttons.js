b3d0af03c08ec6beab42d07e044d67c3f4923093
DasPrombelBread <82669166+DasPrombelBread@users.noreply.github.com>
2021-08-13 17:53:22 +0200

    Update buttons.js

--- style/js/buttons.js
+++ style/js/buttons.js
@@ -3,23 +3,23 @@ function twitter() {
               "https://twitter.com/das_bread", "_blank");
 }
 function contact() {
-  location.replace("https://fh-site.github.io/info/contact.html ")
+  location.replace("https://fh-site.github.io/de/info/contact.html ")
 }
 function blog() {
-  location.replace("https://fh-site.github.io/blog/index.html ")
+  location.replace("https://fh-site.github.io/de/blog/index.html ")
 }
 function home() {
-  location.replace("https://fh-site.github.io/home.html")
+  location.replace("https://fh-site.github.io/de/home.html")
 }
 function home_rr() {
   window.open(
               "https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
 }
 function legal() {
-  location.replace("https://fh-site.github.io/info/legal.html ")
+  location.replace("https://fh-site.github.io/de/info/legal.html ")
 }
 function projects() {
-  location.replace("https://fh-site.github.io/info/projects.html ")
+  location.replace("https://fh-site.github.io/de/info/projects.html ")
 }
 function github() {
   window.open(
